85c990b1823d531af9d41df77a75514f36848bf2
eccentricality <youngmin13@gmail.com>
2021-06-29 21:13:43 -0400

    created inquirer prompt for initial manager to lead team

--- index.js
+++ index.js
@@ -4,13 +4,41 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 const inquirer = require('inquirer');
+// import fs to generate html
+const fs = require('fs');
+// html file path variable
+const generatedHtml = './'
 
-// create intern to test
-let intern = new Intern('Anakin','19','futuresithlord@jedi.com','Coruscant')
-
-// console.log to test intern
-console.log(intern.returnName());
-console.log(intern.returnId());
-console.log(intern.returnEmail());
-console.log(intern.returnRole());
-console.log(intern.returnSchool());
+// generate questions via inquirer to create initial manager to lead team
+inquirer.prompt([
+    {
+        name: 'managerName',
+        type: 'input',
+        message: 'Enter the Manager\'s Name:',
+    },
+    {
+        name: 'managerId',
+        type: 'input',
+        message: 'Enter the Manager\'s ID:',
+    },
+    {
+        name: 'managerEmail',
+        type: 'input',
+        message: 'Enter the Manager\'s Email:',
+    },
+    {
+        name: 'managerOfficeNum',
+        type: 'input',
+        message: 'Enter the Manager\'s Office Number:',
+    },
+    {
+        name: 'managerTeamList',
+        type: 'list',
+        message: 'Who would you like to add to this team?',
+        choices: ['Engineer', 'Intern']
+    }
+])
+.then(response => {
+    // create new file or overwrite if it exists
+    fs.writeFileSync(generatedHtml, '');
+})
