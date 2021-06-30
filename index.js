f80a492e341a6957741a5707c59cf00d8f2207c7
eccentricality <youngmin13@gmail.com>
2021-06-29 20:53:26 -0400

    added extended classes from employee parent and tested a new intern

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 const inquirer = require('inquirer');
+
+// create intern to test
+let intern = new Intern('Anakin','19','futuresithlord@jedi.com','Coruscant')
+
+// console.log to test intern
+console.log(intern.returnName());
+console.log(intern.returnId());
+console.log(intern.returnEmail());
+console.log(intern.returnRole());
+console.log(intern.returnSchool());
