8dd1df8588f401619e94e3549cbb4cc9af36eeb2
eccentricality <youngmin13@gmail.com>
2021-06-29 20:40:31 -0400

    created employee class

---
+++ index.js
@@ -0,0 +1,6 @@
+// import classes & inquirer
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+const inquirer = require('inquirer');
